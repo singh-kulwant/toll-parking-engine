7a47fa2a26b1e85b15192eec27193da24ecc555d
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-10-18 19:34:42 +0530

    18 Oct created spring boot project with gradle

---
+++ src/test/java/com/tpl/TollParkingLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollParkingLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
