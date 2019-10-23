f83c4c5b329efdcae8d2f616b1f75fbb72bf320d
Kulwant Singh <singh.kulwant@gmx.com>
2019-10-23 16:53:12 +0530

    removed unused import

--- src/main/java/com/parking/app/TollParkingApplication.java
+++ src/main/java/com/parking/app/TollParkingApplication.java
@@ -2,7 +2,6 @@ package com.parking.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.parking.config.ParkingConfigurationLoader;
