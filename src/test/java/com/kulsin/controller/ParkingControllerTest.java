11be9d9cca0e29642aab4c6ccc294f23678e011b
Kulwant Singh <kulwant.singh@leovegas.com>
2024-04-28 09:40:07 +0200

    Added tests

---
+++ src/test/java/com/kulsin/controller/ParkingControllerTest.java
@@ -0,0 +1,220 @@
+package com.kulsin.controller;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.kulsin.models.response.ParkingStatus;
+import com.kulsin.repository.ParkedVehicleRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.kulsin.models.VehicleType.GASOLINE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private ParkedVehicleRepository repository;
+
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+    public void setUp() {
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        configureFor("localhost", wireMockServer.port());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        repository.deleteAll();
+        wireMockServer.stop();
+    }
+
+    @Test
+    public void testParkEndpoint() {
+        webTestClient.post()
+                .uri("/api/park")
+                .contentType(APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                        "plateNumber": "ABC123",
+                        "vehicleType": "gasoline",
+                        "color": "red",
+                        "brand": "Toyota"
+                        }""")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(true)
+                .jsonPath("$.message").isEqualTo("Parked successfully");
+    }
+
+    @Test
+    public void testUnparkEndpoint() {
+        webTestClient.post()
+                .uri("/api/unpark")
+                .contentType(APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                        "plateNumber":"ABC123"
+                        }""")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(true)
+                .jsonPath("$.message").isEqualTo("Unparked successfully");
+    }
+
+    @Test
+    public void testGenerateBill() {
+        webTestClient.post()
+                .uri("/api/park")
+                .contentType(APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                        "plateNumber": "ABC123",
+                        "vehicleType": "gasoline",
+                        "color": "red",
+                        "brand": "Toyota"
+                        }""")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(true)
+                .jsonPath("$.message").isEqualTo("Parked successfully");
+
+        webTestClient.post()
+                .uri("/api/generate-bill?plate-number=ABC123")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.billAmount").isEqualTo(5);
+    }
+
+    @Test
+    public void testParkingStatus() {
+        webTestClient.post()
+                .uri("/api/park")
+                .contentType(APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                        "plateNumber": "ABC123",
+                        "vehicleType": "gasoline",
+                        "color": "red",
+                        "brand": "Toyota"
+                        }""")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(true)
+                .jsonPath("$.message").isEqualTo("Parked successfully");
+
+        webTestClient.get()
+                .uri("/api/parking-status")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ParkingStatus.class)
+                .hasSize(1)
+                .consumeWith(entityExchangeResult -> {
+                    List<ParkingStatus> parkingStatusList = entityExchangeResult.getResponseBody();
+                    Assertions.assertFalse(parkingStatusList.isEmpty());
+
+                    ParkingStatus parkingStatus = parkingStatusList.get(0);
+                    assertEquals("GASOLINE_slot_1", parkingStatus.getSlotNumber());
+                    assertEquals("Toyota", parkingStatus.getBrand());
+                    assertEquals("red", parkingStatus.getColor());
+                    assertEquals("ABC123", parkingStatus.getPlateNumber());
+                    assertEquals(GASOLINE, parkingStatus.getVehicleType());
+                });
+
+    }
+
+    @Test
+    public void testSearchVehicle() {
+        webTestClient.post()
+                .uri("/api/park")
+                .contentType(APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                        "plateNumber": "ABC123",
+                        "vehicleType": "gasoline",
+                        "color": "red",
+                        "brand": "Toyota"
+                        }""")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(true)
+                .jsonPath("$.message").isEqualTo("Parked successfully");
+
+        webTestClient.get()
+                .uri("/api/search-vehicle?plate-number=ABC123")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ParkingStatus.class)
+                .hasSize(1)
+                .consumeWith(entityExchangeResult -> {
+                    List<ParkingStatus> parkingStatusList = entityExchangeResult.getResponseBody();
+                    Assertions.assertFalse(parkingStatusList.isEmpty());
+
+                    ParkingStatus parkingStatus = parkingStatusList.get(0);
+                    assertEquals("GASOLINE_slot_1", parkingStatus.getSlotNumber());
+                    assertEquals("Toyota", parkingStatus.getBrand());
+                    assertEquals("red", parkingStatus.getColor());
+                    assertEquals("ABC123", parkingStatus.getPlateNumber());
+                    assertEquals(GASOLINE, parkingStatus.getVehicleType());
+                });
+    }
+
+    @Test
+    public void testSearchByParkingSlot() {
+        webTestClient.post()
+                .uri("/api/park")
+                .contentType(APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                        "plateNumber": "ABC123",
+                        "vehicleType": "gasoline",
+                        "color": "red",
+                        "brand": "Toyota"
+                        }""")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.success").isEqualTo(true)
+                .jsonPath("$.message").isEqualTo("Parked successfully");
+
+        webTestClient.get()
+                .uri("/api/parking-slot/GASOLINE_slot_1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ParkingStatus.class)
+                .hasSize(1)
+                .consumeWith(entityExchangeResult -> {
+                    List<ParkingStatus> parkingStatusList = entityExchangeResult.getResponseBody();
+                    Assertions.assertFalse(parkingStatusList.isEmpty());
+
+                    ParkingStatus parkingStatus = parkingStatusList.get(0);
+                    assertEquals("GASOLINE_slot_1", parkingStatus.getSlotNumber());
+                    assertEquals("Toyota", parkingStatus.getBrand());
+                    assertEquals("red", parkingStatus.getColor());
+                    assertEquals("ABC123", parkingStatus.getPlateNumber());
+                    assertEquals(GASOLINE, parkingStatus.getVehicleType());
+                });
+    }
+
+}
---
+++ src/test/java/com/kulsin/service/ParkingOperationsServiceTest.java
@@ -0,0 +1,164 @@
+package com.kulsin.service;
+
+import com.kulsin.converter.ParkRequestToParkingConverter;
+import com.kulsin.converter.ParkingToParkingStatusConverter;
+import com.kulsin.dto.Parking;
+import com.kulsin.models.VehicleType;
+import com.kulsin.models.request.ParkRequest;
+import com.kulsin.models.request.UnparkRequest;
+import com.kulsin.models.response.GenerateBillResponse;
+import com.kulsin.models.response.ParkUnparkResponse;
+import com.kulsin.models.response.ParkingStatus;
+import com.kulsin.repository.ParkedVehicleRepository;
+import jakarta.persistence.Column;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.GenericConversionService;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class ParkingOperationsServiceTest {
+
+    private GenericConversionService conversionService;
+    private ParkedVehicleRepository repository;
+    private ParkingAdministrationService administrationService;
+    private ParkingOperationsService parkingOperationsService;
+
+    @BeforeEach
+    public void setUp() {
+
+        conversionService = new GenericConversionService();
+        conversionService.addConverter(new ParkingToParkingStatusConverter());
+        conversionService.addConverter(new ParkRequestToParkingConverter());
+
+        repository = mock(ParkedVehicleRepository.class);
+        administrationService = mock(ParkingAdministrationService.class);
+        parkingOperationsService = new ParkingOperationsServiceImpl(conversionService, repository, administrationService);
+    }
+
+    @Test
+    public void testParkVehicle() {
+        // Mock park request
+        ParkRequest parkRequest = new ParkRequest();
+        parkRequest.setPlateNumber("ABC123");
+        parkRequest.setVehicleType("CAR");
+
+        // Mock administration service
+        when(administrationService.getSlotNumber(any())).thenReturn("A1");
+
+        // Perform park operation
+        ParkUnparkResponse response = parkingOperationsService.parkVehicle(parkRequest);
+
+        // Verify the result
+        assertTrue(response.isSuccess());
+        assertEquals("Parked successfully", response.getMessage());
+    }
+
+    @Test
+    public void testUnparkVehicle() {
+        // Mock unpark request
+        UnparkRequest unparkRequest = new UnparkRequest();
+        unparkRequest.setPlateNumber("ABC123");
+
+        // Perform unpark operation
+        ParkUnparkResponse response = parkingOperationsService.unparkVehicle(unparkRequest);
+
+        // Verify the result
+        assertTrue(response.isSuccess());
+        assertEquals("Unparked successfully", response.getMessage());
+
+        // Verify if deleteByVehicleRegistration method is called with correct parameter
+        verify(repository).deleteByVehicleRegistration("ABC123");
+    }
+
+    @Test
+    public void testGenerateBill() {
+        // Mock parking object
+        Parking parking = mockParking("ABC123");
+
+        // Mock repository method
+        when(repository.findParkingByVehicleRegistration("ABC123")).thenReturn(Optional.of(parking));
+
+        // Mock administration service
+        when(administrationService.calculateBill(parking)).thenReturn(100L);
+
+        // Perform generateBill operation
+        GenerateBillResponse response = parkingOperationsService.generateBill("ABC123");
+
+        // Verify the result
+        assertNotNull(response);
+        assertEquals(100L, response.getBillAmount());
+    }
+
+    @Test
+    public void testCurrentParkingLotStatus() {
+        // Mock parking objects
+        Parking parking1 = mockParking("ABC123");
+        Parking parking2 = mockParking("DEF456");
+
+        // Mock repository method
+        when(repository.findAll()).thenReturn(List.of(parking1, parking2));
+
+        // Perform currentParkingLotStatus operation
+        List<ParkingStatus> statusList = parkingOperationsService.currentParkingLotStatus();
+
+        // Verify the result
+        assertNotNull(statusList);
+        assertEquals(2, statusList.size());
+    }
+
+    @Test
+    public void testSearchVehicleByRegistration() {
+        // Mock parking object
+        Parking parking =  mockParking("ABC123");
+
+        // Mock repository method
+        when(repository.findParkingByVehicleRegistration("ABC123")).thenReturn(Optional.of(parking));
+
+        // Perform searchVehicleByRegistration operation
+        ParkingStatus status = parkingOperationsService.searchVehicleByRegistration("ABC123");
+
+        // Verify the result
+        assertNotNull(status);
+        assertEquals("ABC123", status.getPlateNumber());
+    }
+
+    @Test
+    public void testGetParkingSlotDetails() {
+        // Mock parking object
+        Parking parking =  mockParking("ABC123");
+
+        // Mock repository method
+        when(repository.findBySlotNumber("GASOLINE_slot_1")).thenReturn(Optional.of(parking));
+
+        // Perform getParkingSlotDetails operation
+        ParkingStatus status = parkingOperationsService.getParkingSlotDetails("GASOLINE_slot_1");
+
+        // Verify the result
+        assertNotNull(status);
+        assertEquals("GASOLINE_slot_1", status.getSlotNumber());
+    }
+
+    private static Parking mockParking(String vehicleRegistration) {
+        Parking parking = new Parking();
+        parking.setVehicleType(VehicleType.GASOLINE.getName());
+        parking.setBrand("Toyota");
+        parking.setColor("Red");
+        parking.setSlotNumber("GASOLINE_slot_1");
+        parking.setEntryTime(Date.from(Instant.now()));
+        parking.setVehicleRegistration(vehicleRegistration);
+        return parking;
+    }
+
+}
